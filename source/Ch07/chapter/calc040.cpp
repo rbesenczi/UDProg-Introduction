6234549b485f0ff7410e0f314505e4d8eeb34615
Renátó Besenczi <renato.besenczi@gmail.com>
2020-10-14 10:34:02 +0200

    Upload Chapter 7 sources and drill file

---
+++ source/Ch07/chapter/calc040.cpp
@@ -0,0 +1,205 @@
+#include "../../std_lib_facilities.h"
+
+constexpr char number = '8';
+constexpr char quit = 'q';
+constexpr char print = ';';
+
+double expression();
+
+class Token{
+public:
+	char kind;
+	double value;
+	Token(char ch): kind(ch), value(0) {}
+	Token(char ch, double val): kind(ch), value(val) {}
+};
+
+class Token_stream {
+public:
+	Token_stream();
+	Token get();
+	void putback(Token t);
+	void ignore(char c);
+private:
+	bool full;
+	Token buffer;
+};
+
+Token_stream::Token_stream() :full(false), buffer(0) {}
+
+void Token_stream::putback(Token t){
+	if (full) error("putback() into full buffer");
+	buffer = t;
+	full = true;
+}
+
+Token Token_stream::get(){
+
+	if (full){
+		full = false;
+		return buffer;
+	}
+
+	char ch;
+	cin >> ch;
+
+	switch (ch){
+		case print:
+		case quit:
+		case '(':
+		case ')':
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%':
+			return Token(ch);
+		case '.':
+		case '0': case '1': case '2': case '3': case '4':
+    	case '5': case '6': case '7': case '8': case '9':
+    	{
+    		cin.putback (ch);
+    		double val;
+    		cin >> val;
+    		return Token(number, val);
+    	}
+    	default: 
+    		error("Bad token");
+	}
+}
+
+void Token_stream::ignore(char c){
+
+	if (full && c == buffer.kind){
+		full = false;
+		return;
+	}
+	full = false;
+
+	char ch = 0;
+	while (cin>>ch)
+		if (ch==c) return;
+}
+
+Token_stream ts;
+
+double primary(){
+
+	Token t = ts.get();
+	switch (t.kind){
+		case '(':
+		{
+			double d = expression();
+			t = ts.get();
+			if (t.kind != ')') error ("')' expected");
+			return d;
+		}
+		case number:
+			return t.value;
+		case '-':
+			return - primary();
+		case '+':
+			return primary();
+		default:
+			error("primary expected");
+	}
+}
+
+double term(){
+
+	double left = primary();
+	Token t = ts.get();
+	while(true){
+		switch (t.kind){
+			case '*':
+				left *= primary();
+				t = ts.get();
+				break;
+			case '/':
+			{
+				double d = primary();
+				if (d == 0) error ("divide by zero");
+				left /= d;
+				t = ts.get();
+				break;
+			}
+			case '%':
+			{
+				double d = primary();
+				if (d == 0) error ("%: Zero oszto");
+				left = fmod (left, d);
+				t = ts.get();
+				break;
+				/*
+				int i1 = narrow_cast<int>(left);
+				int i2 = narrow_cast<int>(primary());
+				if (i2 == 0) error ("%: Zero oszto");
+				left = i1 % i2;
+				t = ts.get();
+				break;
+				*/
+			}
+			default:
+				ts.putback(t);
+				return left;
+		}
+	}
+}
+
+double expression(){
+
+	double left = term();
+	Token t = ts.get();
+	while (true){
+		switch(t.kind){
+			case '+':
+				left += term();
+				t = ts.get();
+				break;
+			case '-':
+				left -= term();
+				t = ts.get();
+				break;
+			default:
+				ts.putback(t);
+				return left;
+		}
+	}
+}
+
+void clean_up_mess(){
+	ts.ignore(print);
+}
+
+void calculate(){
+	
+	double val = 0;
+
+	while (cin)
+		try {
+
+		Token t = ts.get();
+		while (t.kind == print) t = ts.get();
+		if (t.kind == quit) return;
+		ts.putback(t);
+		cout << "=" << expression() << endl;
+	}
+	catch (exception& e) {
+		cerr << e.what() << endl;
+		clean_up_mess();
+	}
+}
+
+int main() 
+
+try {
+	calculate();
+
+	return 0;
+} catch (exception& e){
+	cerr << e.what() << endl;
+	return 1;
+} catch (...) {
+	cerr << "exception\n";
+	return 2;
+}
